648c3f1fa3e81f06fe10e8240188c9ed2de24a0c
Jimmy in Rain <quangminhit94@gmail.com>
2019-12-28 18:46:29 +0700

    First commit

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const cors = require('cors')
+const mysql = require('mysql')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port port!`))
---
+++ server/pro.js
@@ -0,0 +1,2 @@
+var fs = require('fs')
+const cors = require('cors')
